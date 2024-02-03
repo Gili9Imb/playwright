bf7d69cc9c6ae37b9c2cfea8fc20b90c7f6be303
Gili9Imb <gimberman3010@gmail.com>
2024-02-03 21:50:18 +0200

    Update main.py
    change text

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ with sync_playwright() as p:
     browser = p.chromium.launch(headless=False, slow_mo=3*1000)
     page = browser.new_page()
     page.goto("http:www.google.com")
-    print("Open google with chrome")
+    print("Open google with chrome browser")
     page.fill("//input[@id='input']", "something")
