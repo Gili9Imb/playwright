f92691b4e464bc028bf0d414a21d0aabcaea3828
Gili9Imb <gimberman3010@gmail.com>
2024-02-09 10:16:21 +0200

    first completed test
    first completed test

--- main.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False, slow_mo=3*1000)
-    page = browser.new_page()
-    page.goto("http:www.google.com")
-    print("Open google with chrome browser")
-    page.fill("//input[@id='input']", "something")
