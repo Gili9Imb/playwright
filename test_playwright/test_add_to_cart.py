51600aedfcecd75a7c53911073363b431c0297c6
Gili9Imb <gimberman3010@gmail.com>
2025-03-07 20:04:59 +0200

    fix git ignor + change tests
    fix git ignor + change tests

--- test_playwright/test_add_to_cart.py
+++ test_playwright/test_add_to_cart.py
@@ -1,3 +1,5 @@
+from operator import index
+
 from test_actions.Scenarios import Scenarios
 from test_playwright.test_base import BaseTest
 
@@ -13,6 +15,7 @@ class TestAddToCart(BaseTest):
         assert badge.inner_text() == "1", "Cart badge does not contain the expected number"
 
     def testAddAllItemsToCart(self):
+        global button_text
         page = self.get_page()
         Scenarios.login(self, page)
         item_count = clickOnItemsInList(page)
@@ -20,6 +23,14 @@ class TestAddToCart(BaseTest):
         badge = page.locator('[data-test="shopping-cart-badge"]')
         assert badge.is_visible(), "Cart badge is not visible"
         assert int(badge.inner_text()) == item_count, f"Expected {item_count} items in the cart badge"
+
+        buttons = page.locator(".btn_inventory").element_handles()
+        button_texts = []
+
+        for index, button in enumerate(buttons):
+            button_text_after = button.inner_text()
+            button_texts.append(button_text_after)
+        assert button_texts == ["Remove", "Remove", "Remove", "Remove", "Remove", "Remove"],f"The buttons texts are {button_texts}"
 
     def testAddAllItemsToCartAndRemove(self):
         page = self.get_page()
@@ -33,6 +44,13 @@ class TestAddToCart(BaseTest):
 
         clickOnItemsInList(page)
         assert not badge.is_visible(), "Cart badge is still visible after removing items"
+        buttons = page.locator(".btn_inventory").element_handles()
+        button_texts = []
+
+        for index, button in enumerate(buttons):
+            button_text_after = button.inner_text()
+            button_texts.append(button_text_after)
+        assert button_texts == ["Add to cart", "Add to cart", "Add to cart", "Add to cart", "Add to cart", "Add to cart"],f"The buttons texts are {button_texts}"
 
 
 def clickOnItemsInList(page):
