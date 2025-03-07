377ad886a6119490b8879eb4ab8c3ef527456995
Gili9Imb <gimberman3010@gmail.com>
2025-03-07 14:51:31 +0200

    add new test + update git ignore
    add new test + update git ignore

--- test_playwright/test_add_to_cart.py
+++ test_playwright/test_add_to_cart.py
@@ -5,6 +5,21 @@ from test_playwright.test_base import BaseTest
 class TestAddToCart(BaseTest):  # Fixed typo in class name
 
     def testClickOnItemBackPack(self):
-        page = self.get_page()  # Get the Playwright page instance once
-        Scenarios.login(self, page)  # Pass the existing page variable
-        self.click_action.clickOnElementByLocator(page, '[data-test="add-to-cart-sauce-labs-backpack"]')
+        page = self.get_page()
+        Scenarios.login(self, page)
+        page.locator(".btn_inventory").first.click()
+        badge = page.locator('[data-test="shopping-cart-badge"]')
+        assert badge.is_visible(), "Cart badge is not visible"
+        assert badge.inner_text() == "1", "Cart badge does not contain the expected number"
+
+    def testAddAllItemsToCart(self):
+            page = self.get_page()
+            Scenarios.login(self, page)
+            buttons = page.locator(".btn_inventory").element_handles()
+            for button in buttons:
+                button.click()
+
+            badge = page.locator('[data-test="shopping-cart-badge"]')
+            button_count = len(buttons)
+            assert badge.is_visible(), "Cart badge is not visible"
+            assert int(badge.inner_text()) == button_count, f"Cart badge does not contain the expected number {button_count}"
