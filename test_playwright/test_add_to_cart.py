a676748b638e65b64dd77bf0e2b9c4912cd66982
Gili9Imb <gimberman3010@gmail.com>
2025-03-24 21:05:29 +0200

    change all to get locator methods

--- test_playwright/test_add_to_cart.py
+++ test_playwright/test_add_to_cart.py
@@ -1,16 +1,15 @@
-from operator import index
-
+from test_actions.GetLocator import getLocator
 from test_actions.Scenarios import Scenarios
 from test_playwright.test_base import BaseTest
-
 
 class TestAddToCart(BaseTest):
 
     def testClickOnItemBackPack(self):
         page = self.get_page()
         Scenarios.login(self, page)
-        page.locator(".btn_inventory").first.click()
-        badge = page.locator('[data-test="shopping-cart-badge"]')
+        getLocator(page, "locator", ".btn_inventory").first.click()
+
+        badge = getLocator(page, "locator", '[data-test="shopping-cart-badge"]')
         assert badge.is_visible(), "Cart badge is not visible"
         assert badge.inner_text() == "1", "Cart badge does not contain the expected number"
 
@@ -20,11 +19,11 @@ class TestAddToCart(BaseTest):
         Scenarios.login(self, page)
         item_count = clickOnItemsInList(page)
 
-        badge = page.locator('[data-test="shopping-cart-badge"]')
+        badge = getLocator(page, "locator", '[data-test="shopping-cart-badge"]')
         assert badge.is_visible(), "Cart badge is not visible"
         assert int(badge.inner_text()) == item_count, f"Expected {item_count} items in the cart badge"
 
-        buttons = page.locator(".btn_inventory").element_handles()
+        buttons = getLocator(page,"locator",".btn_inventory").element_handles()
         button_texts = []
 
         for index, button in enumerate(buttons):
@@ -38,13 +37,14 @@ class TestAddToCart(BaseTest):
 
         item_count = clickOnItemsInList(page)
 
-        badge = page.locator('[data-test="shopping-cart-badge"]')
+        badge = getLocator(page, "locator", '[data-test="shopping-cart-badge"]')
         assert badge.is_visible(), "Cart badge is not visible after adding items"
         assert int(badge.inner_text()) == item_count, f"Expected {item_count} items in the cart badge"
 
         clickOnItemsInList(page)
         assert not badge.is_visible(), "Cart badge is still visible after removing items"
-        buttons = page.locator(".btn_inventory").element_handles()
+        buttons = getLocator(page,"locator",".btn_inventory").element_handles()
+
         button_texts = []
 
         for index, button in enumerate(buttons):
--- test_playwright/test_base.py
+++ test_playwright/test_base.py
@@ -1,16 +1,10 @@
 import unittest
 from playwright.sync_api import sync_playwright
-from test_actions.ClickAction import ClickAction
-from test_actions.FillAction import FillAction
 from test_actions.OpenUrl import OpenUrl
-from test_actions.WaitAction import WaitAction
 
 class BaseTest(unittest.TestCase):
 
     def setUp(self):
-        self.fill_action = FillAction()
-        self.click_action = ClickAction()
-        self.wait_action = WaitAction()
         self.open_url = OpenUrl()
         self.playwright = sync_playwright().start()
         self.browser = self.playwright.chromium.launch(headless=False, slow_mo=1200)
