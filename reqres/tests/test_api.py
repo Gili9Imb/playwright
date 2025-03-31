092270cc3f3330e58e9094f8fd90e3147b758f5c
Gili9Imb <gimberman3010@gmail.com>
2025-03-31 12:44:27 +0300

    enhance test

--- reqres/tests/test_api.py
+++ reqres/tests/test_api.py
@@ -5,36 +5,45 @@ from reqres.tests.Scenarios import Scenarios
 from Infra.test_base import BaseTest
 
 class TestAPI(BaseTest):
-    RESPONSE_FILE = r"C:\Users\gnimb\Dev\playwright\playwright\reqres\expected_response.json"
+
+    def __init__(self, methodName: str = "runTest"):
+        super().__init__(methodName)
 
     def setUp(self):
         super().setUp()
         self.page = self.get_page()
         Scenarios.openPage(self, self.page)
 
-    def save_response_to_file(self, data):
-        with open(self.RESPONSE_FILE, "w", encoding="utf-8") as f:
+    def save_response_to_file(self, data, filePath):
+        with open(filePath, "w", encoding="utf-8") as f:
             json.dump(data, f, indent=4)
-        print(f"API response saved to {self.RESPONSE_FILE}")
+        print(f"API response saved to {filePath}")
 
-    def load_expected_response(self):
-        if not os.path.exists(self.RESPONSE_FILE):
-            raise FileNotFoundError(f"Expected response file '{self.RESPONSE_FILE}' not found.")
-        with open(self.RESPONSE_FILE, "r", encoding="utf-8") as f:
+    def load_expected_response(self,filePath):
+        if not os.path.exists(filePath):
+            raise FileNotFoundError(f"Expected response file '{filePath}' not found.")
+        with open(filePath, "r", encoding="utf-8") as f:
             return json.load(f)
 
-    def test(self):
-        getLocator(self.page, "text", "LIST USERS").scroll_into_view_if_needed()
-
-        with self.page.expect_response("**/api/users?page=2") as response_info:
-            getLocator(self.page, "text", "LIST USERS").click()
+    def listen_to_response_after_click(self,endpoint,clickMethod,clickLocator,filePath):
+        with self.page.expect_response(endpoint) as response_info:
+            getLocator(self.page, clickMethod, clickLocator).click()
 
         response = response_info.value
         json_response = response.json()
 
-        if not os.path.exists(self.RESPONSE_FILE):
-            self.save_response_to_file(json_response)
+        if not os.path.exists(filePath):
+            self.save_response_to_file(json_response,filePath)
 
-        expected_response = self.load_expected_response()
+        expected_response = self.load_expected_response(filePath)
         self.assertEqual(json_response, expected_response, f"API response does not match expected JSON.\nReceived: {json.dumps(json_response, indent=2)}")
         print("API response matches expected JSON.")
+
+    def test_response_list_users(self):
+        response_file = r"C:\Users\gnimb\Dev\playwright\playwright\reqres\expected_response.json"
+        end_point = "**/api/users?page=2"
+
+        getLocator(self.page, "text", "LIST USERS").scroll_into_view_if_needed()
+        self.listen_to_response_after_click(end_point, "text","LIST USERS",response_file)
+
+
